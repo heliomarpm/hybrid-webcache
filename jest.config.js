d97a315ad5fe30f37717f933e0f177e1f74d6a90
Heliomar P. Marques <heliomarpm@gmail.com>
2024-12-07 11:43:27 -0300

    ci: tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node',
+    testEnvironment: 'jsdom', //para testes localstorage e indexeddb
     roots: ['<rootDir>/tests'],
+    // setupFiles: [
+    //     "fake-indexeddb/auto"
+    // ],
     coveragePathIgnorePatterns: [
         "/node_modules/",
         "dist"
